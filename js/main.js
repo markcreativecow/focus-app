ee58ad3962cb78dd5956e134dee9a6f3eb231366
Mark Ascroft <mark@hownowcreativecow.co.uk>
2014-03-18 12:46:49 +0000

    Tweaks

--- js/main.js
+++ js/main.js
@@ -6,5 +6,5 @@ function hidePreloader() {
 }
 $(window).load(function() {
     showPreloader();
-	window.open('http://www.learningchallengecurriculum.com/new/assets/resources/progression-in-the-national-curriculum-2014.pdf', '_self', 'location=no,EnableViewPortScale=yes');
+	window.open('pdf/progression-in-the-national-curriculum-2014.pdf', '_self', 'location=no,EnableViewPortScale=yes');
 });
