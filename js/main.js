0c0b7bcbd9ffdcee637d4da4be57a636487bfe8e
Mark Ascroft <mark@hownowcreativecow.co.uk>
2014-05-02 14:22:30 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -5,9 +5,6 @@ function hidePreloader() {
     $('#busy').hide();
 }
 $(window).load(function() {
-    showPreloader();
-	setTimeout(function(){
-		hidePreloader();
-		window.open('pdf/progression-in-the-national-curriculum-2014-app-version.pdf', '_blank', 'location=yes');
-	}, 3000);
+	var devicePlatform = device.platform;
+	alert('asdasd' + devicePlatform);
 });
