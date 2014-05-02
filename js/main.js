ad29608ac56b9256e12a1ddbb939d3581d433a95
Mark Ascroft <mark@hownowcreativecow.co.uk>
2014-05-02 12:14:54 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -8,16 +8,7 @@ $(window).load(function() {
     showPreloader();
 	setTimeout(function(){
 		File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() +"/"+ 'pdf/progression-in-the-national-curriculum-2014-app-version.pdf');
-		Intent target = new Intent(Intent.ACTION_VIEW);
-		target.setDataAndType(Uri.fromFile(file),"application/pdf");
-		target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-		Intent intent = Intent.createChooser(target, "Open File");
-		try {
-			startActivity(intent);
-		} catch (ActivityNotFoundException e) {
-			alert('ooooops');
-		}   
-		// hidePreloader();
-		// window.open('pdf/progression-in-the-national-curriculum-2014-app-version.pdf', '_blank', 'location=yes');
+		
+		alert(JSON.stringify(file));
 	}, 3000);
 });
