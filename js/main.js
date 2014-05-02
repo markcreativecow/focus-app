0418fed7d426903f535ee899507dacb0e7e52842
Mark Ascroft <mark@hownowcreativecow.co.uk>
2014-05-02 14:11:08 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -6,13 +6,5 @@ function hidePreloader() {
 }
 $(window).load(function() {
 	var devicePlatform = device.platform;
-	alert(devicePlatform);
-	setTimeout(function(){
-		hidePreloader();
-		if (devicePlatform == 'Android') {
-			window.open('http://docs.google.com/viewer?url=http%3A%2F%2Fwww.focus-education.co.uk%2Fimages%2Fprogression-in-the-national-curriculum-2014-app-version.pdf', '_blank');
-		} else {
-			window.open('pdf/progression-in-the-national-curriculum-2014-app-version.pdf', '_self', 'EnableViewPortScale=yes');
-		}
-	}, 3000);
+	alert('asdasd' + devicePlatform);
 });
